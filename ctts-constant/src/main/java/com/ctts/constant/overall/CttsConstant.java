1feb37f3159d047330c72d2f74cc7333256f196b
yiliaong <yiliang0225@163.com>
2022-10-09 20:55:21 +0800

    feat:add constant class

---
+++ ctts-constant/src/main/java/com/ctts/constant/overall/CttsConstant.java
@@ -0,0 +1,77 @@
+package com.ctts.constant.overall;
+
+/**
+ * 全局统一常量
+ * @author liangbaichuan
+ */
+public class CttsConstant {
+
+    /**
+     * 数字常量
+     * @author liangbaichuan
+     */
+    public static final class NumberConstant{
+
+        public static final int CONSTANT_0 = 0;
+
+        public static final int CONSTANT_1 = 1;
+
+        public static final int CONSTANT_2 = 2;
+
+        public static final int CONSTANT_3 = 3;
+
+        public static final int CONSTANT_4 = 4;
+
+        public static final int CONSTANT_5 = 5;
+
+        public static final int CONSTANT_6 = 6;
+
+        public static final int CONSTANT_7 = 7;
+
+        public static final int CONSTANT_8 = 8;
+
+        public static final int CONSTANT_9 = 9;
+
+        public static final int CONSTANT_10 = 10;
+
+        public static final int CONSTANT_11 = 11;
+
+        public static final int CONSTANT_12 = 12;
+
+        public static final int CONSTANT_13 = 13;
+
+        public static final int CONSTANT_14 = 14;
+
+        public static final int CONSTANT_15 = 15;
+
+        public static final int CONSTANT_16 = 16;
+
+        public static final int CONSTANT_17 = 17;
+
+        public static final int CONSTANT_18 = 18;
+
+        public static final int CONSTANT_19 = 19;
+
+        public static final int CONSTANT_20 = 20;
+
+        public static final int CONSTANT_21 = 21;
+
+        public static final int CONSTANT_22 = 22;
+
+        public static final int CONSTANT_23 = 23;
+
+        public static final int CONSTANT_24 = 24;
+
+        public static final int CONSTANT_25 = 25;
+
+        public static final int CONSTANT_26 = 26;
+
+        public static final int CONSTANT_27 = 27;
+
+        public static final int CONSTANT_28 = 28;
+
+        public static final int CONSTANT_29 = 29;
+
+        public static final int CONSTANT_30 = 30;
+    }
+}
